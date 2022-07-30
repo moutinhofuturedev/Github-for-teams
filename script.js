e5b589cae6fb7d0934d91dccec1dde57050b34a0
Paulo Moutinho <paulo_vicali@icloud.com>
2022-07-30 01:32:54 -0300

    consuming API of github

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const buttonOpen = document.getElementById('open_modal')
 const buttonClosed = document.querySelector('.closed_modal')
 const secondary = document.querySelector('.wrapper_modal')
+const myApi = document.querySelector('#api')
 
 buttonOpen.addEventListener('click', OpenModal)
 function OpenModal() {
@@ -12,4 +13,27 @@ function CloseModal() {
     secondary.classList.add('invisible')
 }
 
+let database = {
+    name: '',
+    bio: '',
+    location: '',
+}
 
+async function fetchApi() {
+    const li = document.createElement('li')
+
+    fetch('https://api.github.com/users/moutinhofuturedev')
+    .then(response => response.json())
+    .then(data => {
+        let newData = (database) = {
+            name: data.name,
+            bio: data.bio,
+            location: data.location
+        }
+
+        return `<ol>
+        ${li} ${newData}
+        </ol>`
+    })
+}
+myApi.textContent = fetchApi()
